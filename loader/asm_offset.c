81fa8cab574d50c2be4bdccac573aef58e379072
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-05-18 20:56:44 +0000

    Allow asm_offset to be always built and configured for x86_64 to allow cross compiles on arm hosts.

--- loader/asm_offset.c
+++ loader/asm_offset.c
@@ -83,9 +83,7 @@ int main(int argc, char **argv) {
             fprintf(file, "%-32s equ " SIZE_T_FMT "; %s\n", values[i].name, values[i].value, values[i].comment);
         }
     } else if (!strcmp(assembler, "GAS")) {
-#ifdef __x86_64__
         fprintf(file, ".set X86_64, 1\n");
-#endif // __x86_64__
         for (size_t i = 0; i < sizeof(values)/sizeof(values[0]); ++i) {
             fprintf(file, ".set %-32s, " SIZE_T_FMT "# %s\n", values[i].name, values[i].value, values[i].comment);
         }
